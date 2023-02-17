4db51b97890e2fa26a085fa95d766c396b041ed3
Iurii Tikhonov <zeffirkins@gmail.com>
2023-02-17 17:33:50 +0300

    Task from 1 lesson

--- StartingPython/main.py
+++ StartingPython/main.py
@@ -1,2 +1,137 @@
-print ("Hello, World!")
-print (2 + 2)
+#a = int(input())
+
+#if ((a % 4 == 0) and (a % 100 != 0)) or (a % 400 == 0):
+#    print("Високосный")
+#else:
+#    print("Обычный")
+
+#str = '''hello
+#Iurii'''
+#print(str)
+#print(len(str))
+
+#'abc' == "abc"
+#"abc" < "ac"
+#'abc' > 'ab'
+
+
+#print("First Line",  '\n\n\n', "Last Line")
+
+#a = "Iurii"
+#b = 'Tikhonov'
+#print (a + b)
+#print (a + '\n' + b)
+
+# Площадь треугольника
+#a = int(input())
+#b = int(input())
+#c = int(input())
+
+#p = (a + b + c) / 2
+#s = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+
+#print (s)
+
+
+# Попадение в интервал
+
+#a = int(input())
+#if -15 < a <= 12:
+#    print(True)
+#elif 14 < a < 17:
+#    print(True)
+#elif 19 <= a:
+#    print(True)
+#else:
+#    print(False)
+
+# Калькулятор
+
+
+#a = float(input())
+#b = float(input())
+#c = input()
+
+#if c == '+':
+#    print(a + b)
+#elif c == '-':
+#    print(a - b)
+#elif c == '*':
+#    print(a * b)
+#elif c == '/' and b == 0:
+#    print("Деление на 0!")
+#elif c == '/':
+#    print(a / b)
+#elif c == 'mod' and b == 0:
+#    print("Деление на 0!")
+#elif c == 'mod':
+#    print(a % b)
+#elif c == 'pow':
+#    print(a ** b)
+#elif c == 'div' and b == 0:
+#    print("Деление на 0!")
+#elif c == 'div' :
+#    print(a // b)
+
+
+# Площадь комнаты
+
+# figure = input()
+#
+# if figure == "треугольник":
+#     a = int(input())
+#     b = int(input())
+#     c = int(input())
+#
+#     p = (a + b + c) / 2
+#     s = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+#
+#     print (s)
+#
+# elif figure == "прямоугольник":
+#     a = int(input())
+#     b = int(input())
+#
+#     print (a * b)
+#
+# elif figure == "круг":
+#     a = int(input())
+#
+#     print (3.14 * (a ** 2))
+
+
+# Max, Min and Other
+
+# a, b, c = int(input()), int(input()), int(input())
+#
+# _max = max(a,b,c)
+# _min = min(a,b,c)
+# _other = (a + b + c) - (_max + _min)
+#
+# print(_max)
+# print(_min)
+# print(_other)
+
+
+# Робот и программисты
+
+# a = int(input())
+#
+# if a % 10 == 1 and a % 100 != 11:
+#     print ( a, " программист")
+# elif (a % 10 == 2 and a % 100 != 12) or (a % 10 == 3 and a % 100 != 13) or (a % 10 == 4 and a % 100 != 14):
+#     print ( a, " программиста")
+# else:
+#     print ( a, " программистов")
+
+# Счастливый билет
+
+# a,b,c,d,e,f = input()
+#
+# sumLeft = int(a) + int(b) + int(c)
+# sumRight = int(d) + int(e) + int(f)
+#
+# if sumLeft == sumRight:
+#     print("Счастливый")
+# else:
+#     print("Обычный")
